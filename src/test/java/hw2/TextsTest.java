bd296c275e7ae018830bb6d4c4f3ad50f8afaf0d
annashivrina <annashivrina@gmail.com>
2018-01-18 00:21:15 +0300

    Homework 2 - first task correction

--- src/test/java/hw2/TextsTest.java
+++ src/test/java/hw2/TextsTest.java
@@ -1,38 +1,30 @@
 package hw2;
 
+import base.TestBase;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class TextsTest {
+public class TextsTest extends TestBase {
+
     @DataProvider(parallel = true)
     public Object[][] correctTexts() {
-        return new Object[][] {
-                {0, "To include good practices\n" +
-                "and ideas from successful\n" +
-                "EPAM projec"},
-                {1, "To be flexible and\n" +
-                        "customizable"},
+        return new Object[][]{
+                {0, "To include good practices and ideas from successful EPAM projec"},
+                {1, "To be flexible and customizable"},
                 {2, "To be multiplatform"},
-                {3, "Already have good base\n" +
-                        "(about 20 internal and\n" +
-                        "some external projects),\n" +
-                        "wish to get more…"}
-                };
+                {3, "Already have good base (about 20 internal and some external projects), wish to get more…"}
+        };
     }
 
     @Test(dataProvider = "correctTexts")
     public void testTexts(int textNumber, String s) {
-        WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://jdi-framework.github.io/tests");
         List<WebElement> textUnderIcons = driver.findElements(By.className("benefit-txt"));
-        Assert.assertEquals(textUnderIcons.get(textNumber).getText(), s);
-        driver.close();
+        Assert.assertEquals(textUnderIcons.get(textNumber).getText().replaceAll("\n", " "), s);
     }
 }
