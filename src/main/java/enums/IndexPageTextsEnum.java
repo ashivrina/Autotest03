9d7b22d2969c4b954a51de574f8bb9699e6800e0
annashivrina <annashivrina@gmail.com>
2018-01-19 00:28:18 +0300

    Enums - texts on Login page

---
+++ src/main/java/enums/IndexPageTextsEnum.java
@@ -0,0 +1,19 @@
+package enums;
+
+public enum IndexPageTextsEnum {
+
+    TEXT_1("To include good practices and ideas from successful EPAM projec"),
+    TEXT_2("To be flexible and customizable"),
+    TEXT_3("To be multiplatform"),
+    TEXT_4("Already have good base (about 20 internal and some external projects), wish to get more…");
+
+    public String text;
+
+    IndexPageTextsEnum(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
